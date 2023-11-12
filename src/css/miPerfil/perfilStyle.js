2b570cc7ea6a1241e5efbfeb0a1744dc19458634
BrayanStevenMorenoG <brasmoreno5@soy.sena.edu.co>
2023-11-11 22:29:29 -0500

    components

---
+++ src/css/miPerfil/perfilStyle.js
@@ -0,0 +1,41 @@
+import {css} from 'lit-element'
+
+
+export default css`
+
+.navbar-brand{
+    font-size: 30px !important;
+    margin-left: 10px;
+    color: white;
+}
+
+.nav-link{
+    color: white;
+}
+
+.menu-h{
+    margin-left: 920px;
+    font-size: 20px;
+    color: white;
+}
+
+
+.btn{
+    margin-left: 32.5%;
+    position:absolute;
+    margin-top: -2.8%;
+}
+
+.col1{
+   margin-left: 10%;
+   margin-top: 10%;
+   
+   
+}
+.col2{
+    margin-left: 15%;
+
+ }
+
+
+`
